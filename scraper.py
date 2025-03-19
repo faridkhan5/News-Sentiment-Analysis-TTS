b2970c101617b2a5ecbfbed95d17ba9c96edd383
faridkhan5 <faridkhan13555@gmail.com>
2025-03-19 20:48:22 +0530

    scraping news articles done

--- scraper.py
+++ scraper.py
@@ -3,28 +3,27 @@ from bs4 import BeautifulSoup
 
 
 def fetch_news_articles(company_name, max_articles=10):
-    # url = f"https://www.google.com/search?q={company}&tbm=nws"
-    base_url = f"https://news.google.com/rss/search?q={company_name}&hl=en-US&gl=US&ceid=US:en"
+    base_url = f"https://economictimes.indiatimes.com/topic/{company_name}"
     response = requests.get(base_url)
 
     if response.status_code != 200:
         print("Failed to fetch news articles")
         return
 
-    soup = BeautifulSoup(response.content, 'xml')
-    items = soup.find_all('item')[:max_articles]
+    soup = BeautifulSoup(response.content, 'html5lib')
+    titles = soup.find_all('a', class_="wrapLines l2")[:max_articles]
+    contents = soup.find_all('p', class_="wrapLines l3")[:max_articles]
+    dates = soup.find_all('time')[:max_articles]
 
     articles = []
-    for item in items:
-        title = item.title.text
-        link = item.link.text
-        pub_date = item.pubDate.text
-        source = item.source.text if item.source else "Unknown"
+    for i in range(len(titles)):
+        title = titles[i].get_text()
+        content = contents[i].get_text()
+        date = dates[i].get_text()
         articles.append({
             'title': title,
-            'link': link,
-            'pub_date': pub_date,
-            'source': source
+            'content': content,
+            'date': date,
         })
     
     return articles
