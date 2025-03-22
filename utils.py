30c6b9a6f9edee61b0704be5ca850b9f6fcfeddd
faridkhan5 <faridkhan13555@gmail.com>
2025-03-23 00:52:05 +0530

    articles scraping updated

--- utils.py
+++ utils.py
@@ -3,34 +3,32 @@ from bs4 import BeautifulSoup
 from transformers import pipeline
 from dotenv import load_dotenv
 from deep_translator import GoogleTranslator
+import torch
 
+
+# to avoid torch "path doesn't exist error"
+torch.classes.__path__ = []
 
 sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
 def fetch_news_articles(company_name, max_articles=10):
-    base_url = f"https://economictimes.indiatimes.com/topic/{company_name}"
+    base_url = f"https://indianexpress.com/about/{company_name}/"
     response = requests.get(base_url)
-
-    if response.status_code != 200:
-        print("Failed to fetch news articles")
-        return
-
     soup = BeautifulSoup(response.content, 'html5lib')
-    titles = soup.find_all('a', class_="wrapLines l2")[:max_articles]
-    contents = soup.find_all('p', class_="wrapLines l3")[:max_articles]
-    dates = soup.find_all('time')[:max_articles]
-
+    
     articles = []
-    for i in range(len(titles)):
-        title = titles[i].get_text()
-        content = contents[i].get_text()
-        date = dates[i].get_text()
+    for div in soup.find_all('div', class_="img-context")[:max_articles]:
+        title = div.find('h3').find('a').text
+        date_p = div.find('p')
+        date = date_p.text
+        content = date_p.find_next_sibling('p').text
+
         articles.append({
             'title': title,
             'content': content,
-            'date': date,
+            'date': date
         })
-    
+        
     return articles
 
 def analyze_sentiment(text):
@@ -64,10 +62,10 @@ def convert_to_speech(client, text, output_file="output.mp3", model="gpt-4o-mini
 
 def hindi_speech(client, text, output_file="comparison_hindi.mp3"):
     """Convert English text to Hindi speech"""
-    # Translate to Hindi
+    # translate to hindi text
     hindi_text = translate_to_hindi(text)
     
-    # Create speech using OpenAI's TTS
+    # create speech
     speech_file = convert_to_speech(client, hindi_text, output_file)
     
     return {
